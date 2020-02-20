68491a85d434455f331c86b3e19b2c718e63eab6
Alvin <sanghup123@naver.com>
2020-02-20 14:20:23 +0900

    Change Router

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,11 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 // lazy-load
 const Home = lazy(() => import('./pages/Home'));
 
 const Root = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Suspense fallback="loading...">
         <Switch>
           <Route exact path="/" component={Home} />
